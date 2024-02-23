bd3e21c013964ea2d1b0a163302b32665259e763
Cris <corsamps@gmail.com>
2024-02-23 10:51:46 -0500

    Initial commit

---
+++ src/test/java/com/crisdev/api/storeapi/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisdev.api.storeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
